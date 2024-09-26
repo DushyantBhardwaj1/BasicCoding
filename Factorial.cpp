5ecdff74372f7501f098d42f8fbdb5468c476449
DushyantBhardwaj1 <negative5544@gmail.com>
2024-09-27 03:20:59 +0530

    first add

---
+++ Factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int factorial(int a){
+    if(a==0) return 1;
+
+    return a*factorial(a-1);
+}
+int main(){
+    int a;
+    cin>>a;
+    int b=factorial(a);
+    cout<<b;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BasicCoding
 I am going to use this Repositry for storring my code work.
+<br>
+Author Dushyant Bhardwaj
