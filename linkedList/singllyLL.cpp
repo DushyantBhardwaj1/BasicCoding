63563bb07e38e75d0339d1256671f2eb41b001a6
DushyantBhardwaj1 <negative5544@gmail.com>
2024-10-05 22:26:00 +0530

    complete single LL

--- linkedList/singllyLL.cpp
+++ linkedList/singllyLL.cpp
@@ -14,6 +14,19 @@ public:
         this->data = data;
         this->next = NULL;
     }
+
+    // descructor
+    ~Node()
+    {
+        int value = this->data;
+        
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
 };
 
 void insertAtHead(Node *&head, int data)
@@ -31,37 +44,107 @@ void insertAtTail(Node *&tail, int data)
     tail = temp;
 }
 
-void insertAtValue(Node *&head, int data,int key)
+void insertAtValue(Node *&head, Node *&tail, int data, int key)
 {
     Node *temp = new Node(data);
-    Node* temp1 = head;
-    while(temp->data!=key){
-        temp=temp->next;
+    Node *temp1 = head;
+    while (temp1->data != key)
+    {
+        temp1 = temp1->next;
     }
-    temp->next=temp1->next;
-    temp1->next=temp;
+    if (temp1 == head)
+    {
+        insertAtHead(head, data);
+        return;
+    }
+    if (temp1 == tail)
+    {
+        insertAtTail(head, data);
+        return;
+    }
+    temp->next = temp1->next;
+    temp1->next = temp;
+}
+
+void insertAtPosition(Node *&head, Node *&tail, int data, int position)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, data);
+        return;
+    }
+    Node *temp = new Node(data);
+    Node *temp1 = head;
+    int cnt = 1;
+    // agar n per daalna hai to n-1 tek jaana hoga
+    while (cnt < position - 1)
+    {
+        temp1 = temp1->next;
+        cnt++;
+    }
+    if (temp1 == tail)
+    {
+        insertAtTail(tail, data);
+        return;
+    }
+    temp->next = temp1->next;
+    temp1->next = temp;
+}
+
+void deleteAtPosition(Node *&head, Node *&tail, int position)
+{
+    Node *curr = head;
+    Node *prev=NULL;
+    
+
+    if (position == 1)
+    {
+        head = head->next;
+        curr->next=NULL;
+        delete curr;
+        return;
+    }
+    int cnt = 1;
+    while (cnt < position)
+    {
+        prev=curr;
+        curr = curr->next;
+        cnt++;
+    }
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
 }
 void print(Node *&head)
 {
     Node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->data << "-->  ";
+        cout << temp->data << " ";
         temp = temp->next;
     }
+    cout << endl;
 }
 int main()
 {
     Node *node1 = new Node(5);
     Node *head = node1;
     Node *tail = node1;
+    print(head);
     insertAtHead(head, 10);
     insertAtHead(head, 20);
-    insertAtHead(head, 30);
-    insertAtHead(head, 40);
+    print(head);
     insertAtTail(tail, 4);
-    insertAtTail(tail, 3);
-    insertAtTail(tail, 2);
-    insertAtValue(head,6,40);
+    print(head);
+    insertAtValue(head, tail, 6, 10);
+    print(head);
+    insertAtPosition(head, tail, 100, 3);
+    insertAtPosition(head, tail, 200, 1);
+    insertAtPosition(head, tail, 300, 8);
+    print(head);
+    insertAtTail(tail, 400);
+    print(head);
+    deleteAtPosition(head,tail,1);
+    deleteAtPosition(head,tail,2);
     print(head);
 }
