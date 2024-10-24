9e58e44872b017bd1b0d690b14e6093a6bd6412f
DushyantBhardwaj1 <negative5544@gmail.com>
2024-10-24 15:19:34 +0530

    linear search

---
+++ Recurrsion/linearSearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[8]={32,45,67,22,56,66,87,54};
+    int n=8;
+    int var;
+    cout<<"Tell me Number to Find";
+    cin>>var;
+    for(int i=0; i<n; i++){
+        if(arr[i]==var){
+            cout<<"Found at the location "<<i;
+            return 0;
+        }
+    }
+    return 0;
+}
